2994c4afe69fff0d14fdf193585ac9fdfc751535
harikrishnalhk <144059589+harikrishnalhk@users.noreply.github.com>
2025-04-12 09:14:42 +0530

    Create ByteSizeTest.java

---
+++ wrangler-api/src/test/java/io/cdap/wrangler/api/parser/ByteSizeTest.java
@@ -0,0 +1,19 @@
+package io.cdap.wrangler;
+
+import io.cdap.wrangler.api.parser.ByteSize;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ByteSizeTest {
+    @Test
+    public void testByteSizeParsing() {
+        assertEquals(0L, new ByteSize("0B").getBytes());
+        assertEquals(1125899906842624L, new ByteSize("1PB").getBytes());
+        assertEquals(1000L, new ByteSize("1KB").getBytes());
+        assertEquals(1024L, new ByteSize("1KIB").getBytes());
+        assertEquals(1500000L, new ByteSize("1.5MB").getBytes());
+        assertEquals(1572864L, new ByteSize("1.5MIB").getBytes());
+        assertEquals(500L, new ByteSize("500B").getBytes());
+    }
+}
